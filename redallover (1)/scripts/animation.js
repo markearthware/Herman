1259ade2c0e838871d339ab03a3c194c704a9621
Mark Short <mark@earthware.co.uk>
2012-06-13 21:39:53 +0100

    basic frontpage done

---
+++ redallover (1)/scripts/animation.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    $('#herman').animate({
+        top: 57
+    }, 1000, 'swing', function () {
+        $('#spice').animate({
+            top: 66
+        }, 
+        1000
+        );
+    });
+
+
+
+});
